1cd8d5f4e074ec42d210f2357f0ed15fff5cfeb2
Dawid Jaworski <dawid.jaworski@kardex.com>
2025-05-12 08:33:22 +0200

    First part of variable matching

--- ConsoleApplication/Triggers/LACondition.cs
+++ ConsoleApplication/Triggers/LACondition.cs
@@ -3,40 +3,48 @@ using ConsoleApplication.Events;
 
 using System.Text.RegularExpressions;
 
-namespace ConsoleApplication.Triggers
+namespace ConsoleApplication.Triggers;
+public class LACondition : ICloneable
 {
-  public class LACondition : ICloneable
+  public required string Label { get; set; }
+  public required LAConfig Config { get; set; }
+
+  public AdditionalOptions Options { get; set; } = AdditionalOptions.MatchCase;
+  public List<LAVariable> Variables { get; set; } = [];
+
+  public bool Satisfied { get; private set; } = false;
+
+  public bool CheckCondition(string line)
   {
-    public required string Label { get; set; }
-    public required LAConfig Config { get; set; }
+    if (string.IsNullOrEmpty(Config?.Value))
+      throw new Exception($"{nameof(LACondition)} matching value cannot be null or empty");
 
-    public AdditionalOptions Options { get; set; } = AdditionalOptions.MatchCase;
-
-    public bool Satisfied { get; private set; } = false;
-
-    public bool CheckCondition(string line)
+    Satisfied = Config.Mode switch
     {
-      if(string.IsNullOrEmpty(Config?.Value))
-        throw new Exception($"{nameof(LACondition)} matching value cannot be null or empty");
+      MatchingType.Simple => line.Contains(Config.Value),
+      MatchingType.Regex => new Regex(Config.Value, RegexOptions.Singleline).IsMatch(line),
+      MatchingType.Event => LAEventManager.EventsBuffered.Any(e => e.Label == Config.Value && e.ConditionsSatisfied && !e.Consumed),
+      _ => throw new Exception($"Mode: {Config.Mode} not supported"),
+    };
 
-      Satisfied = Config.Mode switch
-      {
-        MatchingType.Simple => line.Contains(Config.Value),
-        MatchingType.Regex => new Regex(Config.Value, RegexOptions.Singleline).IsMatch(line),
-        MatchingType.Event => LAEventManager.EventsBuffered.Any(e => e.Label == Config.Value && e.ConditionsSatisfied && !e.Consumed),
-        _ => throw new Exception($"Mode: {Config.Mode} not supported"),
-      };
+    if (Satisfied)
+      foreach (var variable in Variables)
+        variable.Parse(line);
 
-      return Satisfied;
-    }
+    return Satisfied;
+  }
 
-    public void ResetConditionState()
-    {
-      Satisfied = false;
-    }
-    public Object Clone()
-    {
-      return MemberwiseClone();
-    }
+  public void ResetConditionState()
+  {
+    Satisfied = false;
+  }
+  public Object Clone()
+  {
+    return MemberwiseClone();
+  }
+
+  public override string ToString()
+  {
+    return Label + (Satisfied ? "Satisfied" : "Pending");
   }
 }
---
+++ ConsoleApplication/Triggers/LAVariable.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ConsoleApplication.Triggers;
+
+public class LAVariable : ICloneable
+{
+  public required string Label { get; set; }
+  public required string Filter { get; set; }
+
+  public string? Value { get; private set; }
+
+
+  public void Parse(string line)
+  {
+    var regex = new Regex(Filter, RegexOptions.Singleline);
+    var match = regex.Match(line);
+
+    if (!match.Success)
+    {
+      Program.Warn($"Could not match value: {Label}");
+      return;
+    }
+    
+    Value = match.Groups[match.Groups.Count - 1].Value;
+  }
+
+  public object Clone()
+  {
+    return MemberwiseClone();
+  }
+  public override string ToString()
+  {
+    return $"{Label}" + (Value != null ? $"= {Value}" : string.Empty);
+  }
+}
